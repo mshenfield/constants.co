d8d55cc23084df2b8fbc8c29ef0ed7a9a3485f06
Justin Mahon <justinmcnm@gmail.com>
2015-06-06 17:57:36 -0500

    removed commented code. The logo is hidden on the xs grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,5 @@
 angular
   .module('constantsCo', [])
-
-  // .filter('quantityArray', function() {
-  //   return function(constants) {
-  //     if (constants) {
-  //       return constants.Quantity;
-  //     }
-  //   };
-  // })
-
   .controller('Main', function($http) {
     var main = this;
 
@@ -16,10 +7,6 @@ angular
       .get('data.json')
       .success(function(res) {
         main.constantsObject = res.constants;
-        
-        for(var a=0, b=res.constants.length; a<b; a++){
-          main.terms.push(res['constants'][a]['Quantity']);
-        }
       });
 
     main.constantOnDisplay = {};
