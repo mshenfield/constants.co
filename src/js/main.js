b28a1b9a3868532b82f997a4388d0c6340d37d22
Justin Mahon <justinmcnm@gmail.com>
2015-06-06 17:04:02 -0500

    added the search terms into a flat array for the auto complete

--- src/js/main.js
+++ src/js/main.js
@@ -11,18 +11,23 @@ angular
 
   .controller('Main', function($http) {
     var main = this;
-
+    main.terms = [];
 
     $http
       .get('data.json')
       .success(function(res) {
         main.constantsObject = res.constants;
-        console.log(res);
+        
+        for(var a=0, b=res.constants.length; a<b; a++){
+          main.terms.push(res['constants'][a]['Quantity']);
+        }
       });
 
 
     main.searchFilter = function(element) {
       return element.Quantity.match();
     };
+
+   
 
   });
