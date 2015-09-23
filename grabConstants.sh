82fa4f7a035dc7292e2cbc4b9d695f84cec87a7e
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-23 16:22:00 -0500

    installed bower for heroku, changed destination of grabConstants script, and added npm scripts for heroku

--- grabConstants.sh
+++ grabConstants.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl http://www.nist.gov/srd/srd_data/srd121_allascii.json > data.json
+curl http://www.nist.gov/srd/srd_data/srd121_allascii.json > src/data.json
 echo "File has been saved"
